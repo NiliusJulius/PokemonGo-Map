3b09df150a94ee316520cfa0fdb9ec098a500d6a
NiliusJulius <mclaren500@hotmail.com>
2016-07-25 11:37:07 +0200

    Changing exact adress to meters because free google API doesn't allow that many requests

--- pogom/slackOutput.py
+++ pogom/slackOutput.py
@@ -1,6 +1,7 @@
 from .utils import get_pokemon_name
 from pogom.utils import get_args
 from datetime import datetime
+from math import radians, cos, sin, asin, sqrt
 
 import httplib
 import urllib
@@ -12,7 +13,23 @@ ignore = []
 if args.ignore:
     ignore = [i.lower().strip() for i in args.ignore.split(',')]
 alreadyseen = {}
-              
+ 
+def lonlat_to_meters(lat1, lon1, lat2, lon2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    # earth radius in meters: 6378100
+    m = 6378100 * c
+    return m
+ 
 def outputToSlack(id,encounter_id,lat,lng,itime):
     slack_webhook_urlpath = str(args.slack_webhook)
     pokemon_name = get_pokemon_name(id)
@@ -27,7 +44,12 @@ def outputToSlack(id,encounter_id,lat,lng,itime):
         user_icon = args.pokemon_icons + pokemon_name.lower() + ':'
     else:
         user_icon = ':pokeball:'
-
+    
+    loc = [l.strip() for l in args.location.split(',')]
+    #print "heeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeej" + str(loc[0])
+    distance = lonlat_to_meters(float(loc[0]), float(loc[1]), lat, lng)
+    print distance
+        
     time_till_disappears = itime - datetime.now()
     disappear_hours, disappear_remainder = divmod(time_till_disappears.seconds, 3600)
     disappear_minutes, disappear_seconds = divmod(disappear_remainder, 60)
@@ -39,10 +61,11 @@ def outputToSlack(id,encounter_id,lat,lng,itime):
     url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(lat) + "," + str(lng)
     response = urllib.urlopen(url)
     loc_dic = json.loads(response.read())
-    print loc_dic['results'][0]['address_components'][1]['long_name']
-    text = "You can find me at <http://maps.google.com/maps?q=loc:" + str(lat) + "," + str(lng) + \
-            "| " + loc_dic['results'][0]['address_components'][1]['long_name'] + \
-            " " + loc_dic['results'][0]['address_components'][0]['long_name'] + "> until " + disappear_time + \
+    #print loc_dic
+    #print loc_dic['results'][0]['address_components'][1]['long_name']
+    text = "<http://maps.google.com/maps?q=loc:" + str(lat) + "," + str(lng) + \
+            "|" + '{0:.2f}'.format(distance) + \
+            " m> afstand, tot: " + disappear_time + \
             " (" + disappear_minutes + ":" + disappear_seconds + ")!"
     data = urllib.urlencode({'payload': '{"username": "' + pokemon_name + '", '
                                         '"icon_emoji": "' + user_icon + '", '
@@ -59,13 +82,5 @@ def outputToSlack(id,encounter_id,lat,lng,itime):
     alreadyseen[encounter_id] = pokemon_name
     
     
-    
-    
-    #global slack_webhook_urlpath
-    #slack_webhook_urlpath = str(args.slack_webhook)
-
-    #global pokemon_icons_prefix
-    #if args.pokemon_icons:
-    #    pokemon_icons_prefix = args.pokemon_icons
-    #else:
-    #    pokemon_icons_prefix = False
+            #"| " + loc_dic['results'][0]['address_components'][1]['long_name'] + \
+            #" " + loc_dic['results'][0]['address_components'][0]['long_name'] + "> until " + disappear_time + \
